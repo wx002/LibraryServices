9ccea398149820719db368c2134703d41ee8ea83
Ben <wxu22@mail.depaul.edu>
2019-09-24 21:07:28 -0500

    init repo

---
+++ src/main/java/group2/Libaray/Services/LibarayServicesApplication.java
@@ -0,0 +1,13 @@
+package group2.Libaray.Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarayServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarayServicesApplication.class, args);
+	}
+
+}
