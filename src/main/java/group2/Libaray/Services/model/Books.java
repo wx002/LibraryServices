99d096d8cf7f443cb86acb3423004dae73a37e01
Rafael Rejuso <r.rejuso@gmail.com>
2019-10-16 08:07:23 -0500

    Books JPA set up.
    The other content will be simpler, just follow same structure.

--- src/main/java/group2/Libaray/Services/model/Books.java
+++ src/main/java/group2/Libaray/Services/model/Books.java
@@ -5,10 +5,59 @@
  */
 package group2.Libaray.Services.model;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import lombok.ToString;
+
 /**
  *
  * @author RR
  */
-public class Books {
+@Data
+@Entity
+@ToString
+public class Books implements Serializable{
+    
+    private static final long serialVersionUID = 2L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long isbn;
+    
+    private String title;
+    
+    @ManyToOne
+    @JoinColumn(name = "author", nullable = false)
+    private BooksByAuthor author;
+    
+    @OneToMany(
+            mappedBy = "bookGenres",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @ToString.Exclude
+    private List<BooksByGenre> genres;
+    
+    @OneToMany(
+            mappedBy = "bookYear",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @ToString.Exclude
+    private List<BooksByYear> year;
+   
     
 }
