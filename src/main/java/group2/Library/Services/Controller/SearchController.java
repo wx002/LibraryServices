291efc1d9555de009e73bfc5de9b4ce4f210a493
Adam Brown <abrow191@mail.depaul.edu>
2019-11-11 00:26:04 -0600

    Add search by title and author

--- src/main/java/group2/Library/Services/Controller/SearchController.java
+++ src/main/java/group2/Library/Services/Controller/SearchController.java
@@ -1,6 +1,7 @@
 package group2.Library.Services.Controller;
 
 import group2.Library.DBInterfaces.BookRepository;
+import group2.Library.Services.Model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 
 @Controller @RequestMapping("/search")
@@ -22,23 +24,39 @@ public class SearchController {
 
     @GetMapping
     public String showSearchPage(Model model) {
-        model.addAttribute("ISBNholder", new ISBNholder());
+        model.addAttribute("SearchTermHolder", new SearchTermHolder());
         return "search";
     }
 
     @PostMapping
-    public String showISBN(@Valid ISBNholder isbnholder, BindingResult br, Model model) {
+    public String showResults(@Valid SearchTermHolder searchtermholder, BindingResult br, Model model) {
         if(br.hasErrors())
             return "search";
-        model.addAttribute("books", repo.findByisbn(isbnholder.getISBN()));
+
+        ArrayList<Book> books = new ArrayList<Book>();
+        String searchTerm = searchtermholder.getSearchTerm().toLowerCase();
+
+        for(Book b : repo.findAll()) {
+            String title = b.getTitle().toLowerCase();
+            String author = b.getAuthor().getAuthname().toLowerCase();
+            String isbn = b.getIsbn();
+            if(title.contains(searchTerm) || author.contains(searchTerm) || isbn.contains(searchTerm))
+                books.add(b);
+        }
+
+        if(searchTerm.equals(""))
+            books.clear();
+
+        model.addAttribute("books", books);
+
         return "searchResults";
     }
 
-    public class ISBNholder {
-        private String ISBN;
+    public class SearchTermHolder {
+        private String searchTerm;
 
-        public void setISBN(String ISBN) {this.ISBN = ISBN;}
+        public void setSearchTerm(String searchTerm) {this.searchTerm = searchTerm;}
 
-        public String getISBN() {return this.ISBN;}
+        public String getSearchTerm() {return this.searchTerm;}
     }
 }
