817b9fec09111e895390651efb77b823950311ea
Ben <wxu22@mail.depaul.edu>
2019-11-12 21:17:50 -0600

    fix compile error due to JTesting

--- src/test/java/group2/Library/Services/AuthorTests.java
+++ src/test/java/group2/Library/Services/AuthorTests.java
@@ -4,23 +4,37 @@ package group2.Library.Services;
 
 import group2.Library.DBInterfaces.AuthorRepository;
 import group2.Library.DBInterfaces.RoomRepo;
+import group2.Library.DBInterfaces.UserRepo;
+import group2.Library.DBInterfaces.IncomeRepo;
+import group2.Library.DBInterfaces.LoggingRepo;
 import group2.Library.DBInterfaces.RoomRepository;
 import group2.Library.Services.Model.Author;
 import group2.Library.Services.Model.RoomSchedule;
 import java.util.ArrayList;
+import javax.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-public class LibraryServicesApplicationTests {
+//@DataJpaTest
+@ContextConfiguration(classes=LibraryServicesApplication.class)
+@SpringBootTest(classes =Author.class)
+@PropertySource("classpath:application.properties")
+@AutoConfigureTestEntityManager
+public class AuthorTests {
 
         @Autowired
         private AuthorRepository repository;
@@ -30,10 +44,12 @@ public class LibraryServicesApplicationTests {
         @Autowired
         private TestEntityManager entityManager;
         
+
         private static final Logger log = LoggerFactory.getLogger(LibraryServicesApplication.class);
         
         
 	@Test
+        @Transactional
 	public void testAdd() {
             final String AUTHNAME = "George R. R. Martin";
             Author gotAuthor = new Author();
@@ -48,8 +64,6 @@ public class LibraryServicesApplicationTests {
         
         @Test
         public void testRoom(){
-            
-            
         }
 
 }
